715a31a18b74ae452f0a3bd00e171e85235aaf82
Anis Melliti <ansmeliti@gmail.com>
2020-01-14 21:14:04 +0100

    impelement the user schema

---
+++ Backend/Model/User.js
@@ -0,0 +1,17 @@
+'use strict';
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  login: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const User = mongoose.model('user', userSchema);
+export default User;
