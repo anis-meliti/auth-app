b31ccf30d8ed8d3d7e736b9ce4a0fcb127a78f43
Anis Melliti <ansmeliti@gmail.com>
2020-01-14 21:36:19 +0100

    implement the register controller

---
+++ Backend/Controllers/registration.controller.js
@@ -0,0 +1,33 @@
+'use strict';
+
+import bcrypt from 'bcryptjs';
+
+import User from '../Model/User'
+
+const registerController = {
+  register: async (req, res) => {
+    const { login, password } = req.body;
+    try {
+        const searchResult = await User.findOne({login})
+        if(searchResult) return res.status(400).json({msg:'bad credential!'})
+        const newUser = new User({
+            login,
+            password
+        })
+        bcrypt.genSalt(10,(error,salt)=>{
+            error? console.log(error):bcrypt.hash(newUser.password,salt,(err,hash)=>{
+                if(err) throw err
+                newUser.password=hash
+                try {
+                    const addResult = await newUser.save();
+                    res.status(200).json(addResult)
+                } catch (error) {
+                    console.error(error)
+                }
+            })
+        })
+    } catch (error) {
+        console.error(error)
+    }
+  }
+};
--- Backend/Model/User.js
+++ Backend/Model/User.js
@@ -12,6 +12,5 @@ const userSchema = new mongoose.Schema({
     required: true
   }
 });
-
 const User = mongoose.model('user', userSchema);
 export default User;
