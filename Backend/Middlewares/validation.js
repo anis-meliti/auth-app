509d3e929bc7161aeb22d51f6c7733bc9efafc8e
Anis Melliti <ansmeliti@gmail.com>
2020-01-14 21:22:41 +0100

    implement the middleware checkvalidator

---
+++ Backend/Middlewares/validation.js
@@ -0,0 +1,13 @@
+'use strict';
+import { check, validationResult } from 'express-validator';
+
+export const credvalidation = () => [
+  check('login', 'this field is required!').notEmpty(),
+  check('password', 'this field is required').notEmpty(),
+  check('password', 'this field require min length 6 char').isLength({ min: 6 })
+];
+
+export const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  !errors.isEmpty() ? res.status(400).json({ errors: errors.array() }) : next();
+};
